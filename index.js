9320eada68c0118cb756aaa58e3f9306109e6dba
TinglyPants <chalkytangfastics@gmail.com>
2024-07-29 14:45:22 +0100

    Connected post routes to main server

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const multer = require("multer");
 // Allowing for cross-origin resource sharing. (middleware)
 app.use(cors());
 
+// Connect post routes to main server
+const postRoutes = require("./Routes/Post/post");
+app.use("/posts", postRoutes);
+
 // Listen on port 4000
 app.listen(4000, () => {
     console.log("Server listening on port: 4000");
