7195dbaf24bb7a20fc9b38ee739cf67ee3906caa
TinglyPants <chalkytangfastics@gmail.com>
2024-08-07 14:03:59 +0100

    Improved /image route

--- Routes/Media/media.js
+++ Routes/Media/media.js
@@ -4,8 +4,14 @@ const router = express.Router();
 // Node filesystem handling
 const path = require("path");
 const fs = require("fs");
+const { isUuid } = require("uuidv4");
 
 router.get("/image/:imageID", (req, res) => {
+    if (!isUuid(path.parse(req.params.imageID).name)) {
+        // If invalid UUID, reject
+        res.status(400).send("Invalid filename");
+        return;
+    }
     const searchPath = path.join(
         __dirname,
         "../../mediaStorage/image",
