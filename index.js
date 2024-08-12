9fbaf056c17f554e02eba2b04e290fe153c6612d
TinglyPants <chalkytangfastics@gmail.com>
2024-08-12 12:19:22 +0100

    Linked user routes

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ app.use(cors());
 const postRoutes = require("./Routes/Post/post");
 app.use("/posts", postRoutes);
 
+// Connect media routes to main server
 const mediaRoutes = require("./Routes/Media/media");
 app.use("/media", mediaRoutes);
+
+// Connect user routes to main server
+const userRoutes = require("./Routes/User/user");
+app.use("/users", userRoutes);
 
 // Listen on port 4000
 app.listen(4000, () => {
