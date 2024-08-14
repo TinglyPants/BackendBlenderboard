075594a83b3b832e9f4fd4ac7f0114e911ba5a0f
TinglyPants <chalkytangfastics@gmail.com>
2024-08-14 14:55:52 +0100

    Refactored media.js a little

--- Routes/Media/media.js
+++ Routes/Media/media.js
@@ -1,7 +1,6 @@
-// Handles HTTP routes
 const express = require("express");
 const router = express.Router();
-// Node filesystem handling
+
 const path = require("path");
 const fs = require("fs");
 const { isUuid } = require("uuidv4");
@@ -12,63 +11,68 @@ const upload = multer({
     storage: memStorage,
 });
 
+//TODO: Implement security within the media creation endpoints to ensure only the server can create files, not any old user. Like auth protection but even higher access
+// Also must add some functions to reduce duplication.
+
+// READ //////////////////////////////////////////////////////////////
 router.get("/image/:imageID", (req, res) => {
     if (!isUuid(path.parse(req.params.imageID).name)) {
-        // If invalid UUID, reject
         res.status(400).send("Invalid filename");
         return;
     }
+
     const searchPath = path.join(
         __dirname,
         "../../mediaStorage/image",
         req.params.imageID
     );
-    // Check if file exists first
+
     if (fs.existsSync(searchPath)) {
         res.sendFile(searchPath);
     } else {
-        res.send("No file found.");
+        res.status(404).send("No file found.");
     }
 });
 
 router.get("/video/:videoID", (req, res) => {
     if (!isUuid(path.parse(req.params.videoID).name)) {
-        // If invalid UUID, reject
         res.status(400).send("Invalid filename");
         return;
     }
+
     const searchPath = path.join(
         __dirname,
         "../../mediaStorage/video",
         req.params.videoID
     );
-    // Check if file exists first
+
     if (fs.existsSync(searchPath)) {
         res.sendFile(searchPath);
     } else {
-        res.send("No file found.");
+        res.status(404).send("No file found.");
     }
 });
 
 router.get("/model/:modelID", (req, res) => {
     if (!isUuid(path.parse(req.params.modelID).name)) {
-        // If invalid UUID, reject
         res.status(400).send("Invalid filename");
         return;
     }
+
     const searchPath = path.join(
         __dirname,
         "../../mediaStorage/model",
         req.params.modelID
     );
-    // Check if file exists first
+
     if (fs.existsSync(searchPath)) {
         res.sendFile(searchPath);
     } else {
-        res.send("No file found.");
+        res.status(404).send("No file found.");
     }
 });
 
+// CREATE ////////////////////////////////////////////////////////////
 router.post(
     "/image/create/:filename",
     upload.fields([{ name: "image" }]),
